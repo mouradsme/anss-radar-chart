4b4c2fe6bad99d237907ec2bb41ce6f3b55723b8
Mourad Bousserouel <mouradsme@gmail.com>
2022-06-30 17:44:31 +0100

    Updates

--- src/ANSSChartsPanel.tsx
+++ src/ANSSChartsPanel.tsx
@@ -2,57 +2,57 @@ import React, { useEffect } from 'react';
 import { PanelProps } from '@grafana/data';
 import { ChartOptions } from 'types';
 import { Utils  } from 'js/Utils';
-import * as d3 from "d3";
 import $ from 'jquery'
+import Chart from 'chart.js/auto';
 import './css/main.css'
-import { RadialChart } from './chart/init'
 interface Props extends PanelProps<ChartOptions> {}
 
 export const ANSSChartsPanel: React.FC<Props> = (A) => {
-    var   { data, height, width } = A,  
+    var   { data } = A,  
             ChartID       = `Chart-${A.id}`,
 	    	    Series 			  = data.series, 		// Get series from Data Panel data object. Data fetched from a Database/Data Source is stored in the Series Object as a constant
 	 		      Fields 			  = Series[0].fields,	// Fetch Fields (classes), from the series Object
-            Data          = Utils.Populate(Fields, A.options.OrderBy, A.options.OrderDirection === 'desc' ),
-            arcMinRadius  = A.options.arcMinRadius,
-            arcPadding    = A.options.arcPadding,
-            numTicks      = A.options.numTicks,
-            ticksColor    = A.options.TicksColor,
-            backgroundColor = A.options.BackgroundColor,
-            gradientColor1  = A.options.gradientColor1,
-            gradientColor2  = A.options.gradientColor2,
-            minHeight     = 300;
-            useEffect( () => { 
-              height      = height <= minHeight ? $(`#${ChartID}`).height() : height;
-              const chartRadius   = height / 2 - 40
-              RadialChart.init(d3, ChartID, Data, { 
-                numTicks, 
-                chartRadius, 
-                arcMinRadius, 
-                arcPadding, 
-                ticksColor,
-                backgroundColor,
-                gradientColor1,
-                gradientColor2,
-                height, 
-                width},  A.options.autoColoring) 
-            }, [$(`#${ChartID}`)]) // Reload Chart Data once the "container" is ready
-
+            Data          = Utils.Populate(Fields)
+            const Colors  = A.fieldConfig.defaults.thresholds.steps
+            let Datasets  = []
+            Data.series.forEach((Series, i) => {
+              Datasets.push({
+                label: Data.titles[i],
+                data: Series,
+                backgroundColor: ['transparent'],
+                borderColor: Colors[i].color,
+                borderWidth: 1
+              })
+            })
             useEffect(() => {
-              $(`#${ChartID} text.tick`).attr('fill', A.options.TicksColor)
-              $(`#${ChartID} .innerLabels`).find('text').attr('font-size', A.options.fontSize)
-              $(`#${ChartID} .innerLabels`).find('text').each((i, elem) => {
-                let yOri = Number($(elem).attr('yOriginal'))
-                $(elem).attr('x', A.options.labelAxisX)
-                .attr('y', yOri + A.options.labelAxisY)
-                .attr('fill', A.options.innerLabelsColor)
+              const chart = new Chart(ChartID, {
+                type: 'radar',
+                data: {
+                    labels: Data.labels,
+                    datasets: Datasets
 
-              }) 
+                },
+                plugins: [],
+                options: {
+                  
+                  
+                },
+                
+              })
+              const CanvasContainer = $(`[data-panelid=${A.id}]`)
+              let h = CanvasContainer.height() - 50
+              CanvasContainer.find('.RadialChartContainer').css('background-color', A.options.BackgroundColor)
+              chart.canvas.parentElement.style.height = `${h}px`;
+              chart.canvas.parentElement.style.width = `${h}px`;
+              Chart.defaults.color = A.options.LegendTextColor;
+
             }, [A.options])
-            
+
  return (<>
  <div className='tooltip' id={ChartID+'-tooltip'}></div>
-    <div className='RadialChartContainer' id={ChartID}></div>
+    <div className='RadialChartContainer'>
+      <canvas id={ChartID} ></canvas>
+    </div>
  </>) 
 
 };
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,6 @@
-type Orders = 'asc' | 'desc'
 export interface ChartOptions {
-    OrderBy       : any;
-    OrderDirection  : Orders;
-    numTicks      : number;
-    arcMinRadius  : number;
-    arcPadding    : number;
-    TicksColor    : any;
+    LegendTextColor: any;
     BackgroundColor: any;
-    fontSize: number;
-    labelAxisX: number;
-    labelAxisY: number;
-    innerLabelsColor: any;
     gradientColor1: any;
     gradientColor2: any;
     autoColoring: boolean;
